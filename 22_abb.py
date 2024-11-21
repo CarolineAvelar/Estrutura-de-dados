b1b8676321698a5190a30234e03a4783b6eace89
Caroline Avelar <avelarrcaroline@gmail.com>
2024-11-21 10:02:34 -0300

    fazendo testes na Binary Search Tree

--- 22_abb.py
+++ 22_abb.py
@@ -8,3 +8,18 @@ abb.insert(18)
 abb.insert(32)
 abb.insert(35)
 abb.insert(27)
+
+# Percurso em-ordem
+# print é a função (action) que será executada quando
+# o percurso visitar o nó raiz
+print('Em-ordem:')
+abb.in_order_traversal(print)
+print("-" *80)
+
+print('Pré-ordem:')
+abb.pre_order_traversal(print)
+print("-" *80)
+
+print('Pós-ordem:')
+abb.post_order_traversal(print)
+print("-" *80)
